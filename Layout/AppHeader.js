54dd13d6cad84a1f2a7d755bb01ce6bb74f7a825
mhr1380 <mhr.1380.2001@gmail.com>
2023-04-15 02:13:44 +0430

    feat : add posters page

---
+++ Layout/AppHeader.js
@@ -0,0 +1,6 @@
+import classes from "./AppHeader.module.css";
+const AppHeader = () => {
+  return <div className={classes.header}>هدر</div>;
+};
+
+export default AppHeader;
--- pages/posters.js
+++ pages/posters.js
@@ -1,5 +1,394 @@
+import AppHeader from "../Layout/AppHeader";
+import Layout from "../Layout/Layout";
+import bicycle from "../assets/images/bicycle.png";
+import Poster from "../components/Poster";
+import Image from "next/image";
+import classes from "./posters.module.css";
 const Posters = () => {
-  return <></>;
+  return (
+    <>
+      <AppHeader />
+      <div className={classes.sidebar}>سایدبار</div>
+      <div className={classes.container}>
+        <div className={classes.posters_container}>
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>{" "}
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>
+          <div className={classes.poster}>
+            <div className={classes.poster_body}>
+              <div className={classes.poster_title}>دوچرخه</div>
+              <div className={classes.badges_container}>
+                <div className={`${classes.badge} ${classes.emergency}`}>
+                  فوری
+                </div>
+                <div className={`${classes.badge} ${classes.category}`}>
+                  دوچرخه
+                </div>{" "}
+                <div className={`${classes.badge} ${classes.reward}`}>
+                  مژدگانی{" "}
+                </div>
+              </div>
+              <p className={classes.time_description}>
+                3 دقیقه پیش
+                <span className={classes.lost}> گم شده </span>
+                در <b>صادقیه</b>
+              </p>
+            </div>
+            <Image className={classes.poster_image} src={bicycle} width={136} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Posters;
